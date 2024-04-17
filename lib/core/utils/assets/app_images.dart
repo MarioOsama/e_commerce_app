a0f97c7493c78185fc5f9e982fbfa4ee5409fa09
khaled-dreat <dreatkhaled@gmail.com>
2024-04-17 17:49:04 +0300

    Add AppImages class with onboarding screen images

---
+++ lib/core/utils/assets/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  static const String onBoardingScreennbor1 =
+      "assets/img/charlesdeluvio-XdjS0AzKzo0-unsplash.jpg";
+
+  static const String onBoardingScreennbor2 =
+      "assets/img/derick-anies-hDJT_ERrB-w-unsplash.jpg";
+
+  static const String onBoardingScreennbor3 =
+      "assets/img/eduardo-soares-QsYXYSwV3NU-unsplash.jpg";
+
+  static const String onBoardingScreennbor4 =
+      "assets/img/marissa-grootes-D4jRahaUaIc-unsplash.jpg";
+}
