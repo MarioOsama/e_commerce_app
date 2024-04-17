96500d323237c19a45523e3764edcc9256d6a938
khaled-dreat <dreatkhaled@gmail.com>
2024-04-17 17:49:19 +0300

    Refactor asset class names and structure

---
+++ lib/core/utils/assets/app_icons.dart
@@ -0,0 +1,51 @@
+class Assets {
+  Assets._();
+
+  /// Assets for  SvgArrowLeft
+  /// assets/svg/arrow_left.svg
+  static const String arrowLeft = "assets/svg/arrow_left.svg";
+
+  /// Assets for  SvgArrowRight
+  /// assets/svg/arrow_right.svg
+  static const String arrowRight = "assets/svg/arrow_right.svg";
+
+  /// Assets for  SvgCart
+  /// assets/svg/cart.svg
+  static const String cart = "assets/svg/cart.svg";
+
+  /// Assets for  SvgChevronLeft
+  /// assets/svg/chevron_left.svg
+  static const String chevronLeft = "assets/svg/chevron_left.svg";
+
+  /// Assets for  SvgChevronRight
+  /// assets/svg/chevron_right.svg
+  static const String chevronRight = "assets/svg/chevron_right.svg";
+
+  /// Assets for  SvgFilledHeart
+  /// assets/svg/filled_heart.svg
+  static const String filledHeart = "assets/svg/filled_heart.svg";
+
+  /// Assets for  SvgFilter
+  /// assets/svg/filter.svg
+  static const String filter = "assets/svg/filter.svg";
+
+  /// Assets for  SvgHeart
+  /// assets/svg/heart.svg
+  static const String heart = "assets/svg/heart.svg";
+
+  /// Assets for  SvgHome
+  /// assets/svg/home.svg
+  static const String home = "assets/svg/home.svg";
+
+  /// Assets for  SvgProfile
+  /// assets/svg/profile.svg
+  static const String profile = "assets/svg/profile.svg";
+
+  /// Assets for  SvgSearch
+  /// assets/svg/search.svg
+  static const String search = "assets/svg/search.svg";
+
+  /// Assets for  SvgSettings
+  /// assets/svg/settings.svg
+  static const String settings = "assets/svg/settings.svg";
+}
