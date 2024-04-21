8f7f4e2bf5a7418fcf96e55032d7531a9031b083
Mario Osama <114400410+MarioOsama@users.noreply.github.com>
2024-04-21 17:50:01 +0200

    Update app_images.dart

--- lib/core/utils/theming/app_images.dart
+++ lib/core/utils/theming/app_images.dart
@@ -1,12 +1,19 @@
 class AppImages {
   static const String onBoardingScreennbor1 =
-      "assets/img/on_bording_image_1.jpg";
+      "assets/img/charlesdeluvio-XdjS0AzKzo0-unsplash.jpg";
 
   static const String onBoardingScreennbor2 =
-      "assets/img/on_bording_image_2.jpg";
+      "assets/img/derick-anies-hDJT_ERrB-w-unsplash.jpg";
 
   static const String onBoardingScreennbor3 =
-      "assets/img/on_bording_image_3.jpg";
-  static const String sonyHeadphones01 = "assets/img/Sony_Headphones_01.png";
-  static const String visaIcon = "assets/img/visa_icon.png";
+      "assets/img/eduardo-soares-QsYXYSwV3NU-unsplash.jpg";
+
+  static const String onBoardingScreennbor4 =
+      "assets/img/marissa-grootes-D4jRahaUaIc-unsplash.jpg";
+
+  static const String appIconImage = 'assets/img/app_icon_image.png';
+  static const String googleLogo = 'assets/img/Google.svg';
+  static const String appleLogo = 'assets/img/apple.svg';
+
+  static const String facebookLogo = 'assets/img/facebook.svg';
 }
